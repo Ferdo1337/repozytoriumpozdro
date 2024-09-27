189c86accb1fbf5f92ad16897fb3d17a62c4ae0e
Łukasz Wąsik <wasik.lukasz@SZKOLA.ZST-RADOM.EDU.PL>
2024-09-27 16:51:05 +0200

    test

---
+++ pozdro/pozdro/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pozdro;
+    internal class Class1
+    {
+        public string Zadanie1(string data, int indexToCut)
+        {
+            if (data.Length > indexToCut)
+            {
+                return data.Substring(indexToCut);
+            }
+            else
+            {
+                return data;
+            }
+        }
+    }
---
+++ pozdro/pozdro/Program.cs
@@ -0,0 +1,13 @@
+﻿using pozdro;
+
+Console.WriteLine("podaj co chcesz odciac: ");
+string? data = Console.ReadLine();
+
+Console.WriteLine("podaj indeks: ");
+int index = int.Parse(Console.ReadLine());
+
+Class1 cutData = new Class1();
+string result = cutData.Zadanie1(data, index);
+
+Console.WriteLine("Wynik: ");
+Console.WriteLine(result);
